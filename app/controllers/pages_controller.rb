5635711b4316794430415a369a4ccae844d04eaf
Damien Merian <qashdzn@gmail.com>
2024-11-05 16:36:54 +0100

    spotifyAuth: create a model and updated PagesController as well as the view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    if user_signed_in?
+      Spotify.fetch_recent_tracks(current_user)
+      @tracks = current_user.spotify_tracks
+    end
   end
 end

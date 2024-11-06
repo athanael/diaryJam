a58cdb8ba8f3828768ad4c1d1dd5d28f17375bb0
athanael gerard <pilotewpa@gmail.com>
2024-11-06 08:47:50 +0100

    correct importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
-pin "homepage", preload: true, to: "homepage.js"
+pin_all_from 'app/javascript/src', under: 'src', to: 'src'

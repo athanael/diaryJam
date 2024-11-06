5fffc5fde0b25436644fcdcc4029bab64ba8fd68
athanael gerard <pilotewpa@gmail.com>
2024-11-06 09:12:20 +0100

    correct importmap

---
+++ app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="scroll"
+export default class extends Controller {
+  connect() {
+    setTimeout(() => {
+      // Supprimer la classe "no-scroll" du body
+      document.body.classList.remove("no-scroll");
+
+      // Rendre le contenu principal visible et défiler jusqu'à celui-ci
+      const mainContent = this.element.querySelector(".main-content");
+      if (mainContent) {
+        mainContent.style.display = "block";
+
+        window.scrollTo({
+          top: mainContent.offsetTop,
+          behavior: "smooth"
+        });
+      }
+    }, 5000);
+  }
+}

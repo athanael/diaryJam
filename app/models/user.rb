2b9175d765d1de5d644847aa3c3d9985da2ce51c
Imad Belhadri <imad.belhadri@gmail.com>
2024-11-10 17:47:26 +0100

    création de la ProfilePage

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,11 @@ class User < ApplicationRecord
   has_many :tracks, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  def is_connected_to_spotify?
+    !spotify_id.nil?
+  end
+
+  def activity_feed
+    posts.includes(:user_track, :comments).order(created_at: :desc)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :users, only: [:show]
+
+  resources :tracks do
+    post 'like', on: :member
+    resources :comments, only: [:create]
+  end
 end

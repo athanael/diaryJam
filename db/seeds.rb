90e6313ca413649fbcda1655320b5f4e9d603c8b
Damien Merian <qashdzn@gmail.com>
2024-11-10 10:18:35 +0100

    added preview_url

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,15 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+# IDs de morceaux Spotify pour le testing
+track_ids = [
+  "4PTG3Z6ehGkBFwjybzWkR8", # Rick Roll
+  "0VjIjW4GlUZAMYd2vXMi3b", # The Weeknd - Blinding Lights
+  "2ChF0wBv2U5p8gAMnnigre", # Realo - 2 SEC
+  "1YOHWN62HHDszsqg5zQbo7", # kuru - save;File-9
+  "3n4blG5eQN10jGA5LnNt6W"  # Lucy Bedroque - SISTERHOOD (LOVE, HER.)
+]
+
 # Création de 5 utilisateurs
 users = 5.times.map do |i|
   User.find_or_create_by!(
@@ -21,23 +30,19 @@ users = 5.times.map do |i|
   end
 end
 
-# IDs de morceaux Spotify pour le testing
-track_ids = [
-  "4uLU6hMCjMI75M1A2tKUQC", # Rick Roll
-  "1dGr1c8CrMLDpV6mPbImSI", # The Weeknd - Blinding Lights
-  "2P89NOLNfrwXRuotuqDxXr", # Realo - 2 SEC
-  "1YOHWN62HHDszsqg5zQbo7", # kuru - save;File-9
-  "3n4blG5eQN10jGA5LnNt6W"  # Lucy Bedroque - SISTERHOOD (LOVE, HER.)
-]
-
-# Création de 5 tracks + assignation à chaque utilisateur
+# Création de tracks à partir de Spotify
 users.each_with_index do |user, index|
+  spotify_track = RSpotify::Track.find(track_ids[index])
+
   Track.find_or_create_by!(
-    spotify_id: track_ids[index],
+    spotify_id: spotify_track.id,
     user: user
   ) do |track|
-    track.title = Faker::Music::RockBand.song
+    track.title = spotify_track.name
+    track.artist = spotify_track.artists.first.name
+    track.preview_url = spotify_track.preview_url
+    track.image_url = spotify_track.album.images.first['url'] if spotify_track.album.images.any?
   end
 end
 
-puts "Ça a marché !"
+puts "Seeds avec RSpotify terminés !"

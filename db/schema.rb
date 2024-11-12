e64732b6080a5126627cd3f50bd898cb6704d84e
athanael gerard <pilotewpa@gmail.com>
2024-11-12 20:56:04 +0100

    resolve conflicts on schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_12_124617) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_10_091807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_12_124617) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "preview_url"
+    t.string "image_url"
     t.index ["user_id"], name: "index_tracks_on_user_id"
   end
 
@@ -57,7 +59,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_12_124617) do
     t.string "spotify_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "profile_image_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

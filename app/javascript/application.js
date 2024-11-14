fd2b237beb2e57197a799175c3ba51fc8024a3fc
Imad Belhadri <imad.belhadri@gmail.com>
2024-11-14 19:14:23 +0100

    chargement du carousel reglé

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,4 +3,4 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-import "./controllers/carousel";
+import "./controllers/carousel_controller";
---
+++ app/javascript/controllers/carousel_controller.js
@@ -0,0 +1,46 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["trackItem"];
+
+  connect() {
+    console.log("Stimulus carousel controller connecté");
+
+    this.currentIndex = 0;
+
+    this.leftArrow = this.element.querySelector(".left-arrow");
+    this.rightArrow = this.element.querySelector(".right-arrow");
+
+    this.updateCarousel();
+
+    this.rightArrow.addEventListener("click", () => this.nextTrack());
+    this.leftArrow.addEventListener("click", () => this.previousTrack());
+
+    // Défilement automatique
+    this.autoScroll = setInterval(() => this.nextTrack(), 4000);
+  }
+
+  disconnect() {
+    this.rightArrow.removeEventListener("click", () => this.nextTrack());
+    this.leftArrow.removeEventListener("click", () => this.previousTrack());
+    clearInterval(this.autoScroll);
+  }
+
+  updateCarousel() {
+    this.trackItemTargets.forEach((item, index) => {
+      item.style.display = index === this.currentIndex ? "block" : "none";
+    });
+  }
+
+  nextTrack() {
+    console.log("nextTrack appelée");
+    this.currentIndex = (this.currentIndex + 1) % this.trackItemTargets.length;
+    this.updateCarousel();
+  }
+
+  previousTrack() {
+    console.log("previousTrack appelée");
+    this.currentIndex = (this.currentIndex - 1 + this.trackItemTargets.length) % this.trackItemTargets.length;
+    this.updateCarousel();
+  }
+}

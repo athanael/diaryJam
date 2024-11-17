3cf510f7a74d38bae4169e363bef70fdc5d64103
Damien Merian <qashdzn@gmail.com>
2024-11-17 15:05:06 +0100

    Ajout du système de follow avec acts-as-favoritor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,30 @@
 class User < ApplicationRecord
   # Devise modules pour l'authentification
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
 
   # Associations
+  has_many :tracks, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  # Gem acts_as_favoritor pour le système de follow
+  acts_as_favoritor
+  acts_as_favoritable
 
   # Validations
   validates :first_name, :last_name, :username, :email, presence: true
 
-  has_many :tracks, dependent: :destroy
-  has_many :posts, dependent: :destroy
-  has_many :comments, dependent: :destroy
   def is_connected_to_spotify?
     !spotify_id.nil?
   end
 
+  # Méthode pour savoir si un utilisateur suit un autre utilisateur
+  def following?(other_user)
+    self.favorited?(other_user)
+  end
+
+  # Méthode pour le feed d'activité
   def activity_feed
     posts.includes(:user_track, :comments).order(created_at: :desc)
   end
